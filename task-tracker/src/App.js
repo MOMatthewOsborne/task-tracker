42684c3736cad6f1a767b9ea912c2411f691489c
Matthew Osborne <momatthewosborne1@gmail.com>
2023-03-16 20:26:18 +0000

    added components

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,11 +1,39 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Doctors Appointment11",
+      day: "Feb 15th",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Doctors Appointment1111",
+      day: "Feb 25th",
+      reminder: true,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    console.log("delete", id);
+  };
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
-}
+};
 
 export default App;
